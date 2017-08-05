1abd74e546d31fdf8442596388f8ff234bbd2e78
xiaosong <1562631196@qq.com>
2017-08-05 17:06:19 +0800

    配置文件的更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 // var path = require("path");
 module.exports = {
-    entry:"./src/script/main.js",
-    output:{
-        path:__dirname + "/dist/js",
-        filename:"bundle.js"
+    entry:"./src/script/main.js",  //文件入口路径，path还可以是数组用还处理平行不依赖的文件，还可以是对象用来处理多页面的应用
+    output:{                        //打包后出口
+        path:__dirname + "/dist/js",    //出口路径
+        filename:"bundle.js"            //打包后重命名的输出文件，当多文件输出时可以用占位符[name][hash][chunkhash]
+
     }
 }
