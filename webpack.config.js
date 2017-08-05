dfa3c2c4f230337a4680e4dbf8008112b7c10fc7
xiaosong <1562631196@qq.com>
2017-08-05 22:04:03 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-// var path = require("path");
+var htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry:"./src/script/main.js",  //文件入口路径，path还可以是数组用还处理平行不依赖的文件，还可以是对象用来处理多页面的应用
     output:{                        //打包后出口
         path:__dirname + "/dist/js",    //出口路径
-        filename:"bundle.js"            //打包后重命名的输出文件，当多文件输出时可以用占位符[name][hash][chunkhash]
+        filename:"[name]-[chunkhash].js"            //打包后重命名的输出文件，当多文件输出时可以用占位符[name][hash][chunkhash]
 
-    }
+    },
+    plugins:[
+        new htmlWebpackPlugin()
+    ]
 }
