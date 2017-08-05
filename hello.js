893db770175a6aea1abf02963e70e6e8266a083e
xiaosong <1562631196@qq.com>
2017-08-05 15:17:06 +0800

    删除一些文件

--- hello.js
+++
@@ -1,6 +0,0 @@
-require('./word.js');
-require('css-loader!./style.css');//为何不能用style-loader
-
-function hello(){
-    console.log("hello");
-}
--- word.js
+++
@@ -1,3 +0,0 @@
-function word(){
-    console.log("word")
-}
